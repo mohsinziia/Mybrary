df33d356571dd8bfc2e994a26729b38153403965
mohsinziia <ziamohsin409@gmail.com>
2023-08-07 15:02:55 +0500

    Made server changes

--- server.js
+++ server.js
@@ -15,11 +15,18 @@ app.use(express.static("public"))
 app.use("/", indexRouter)
 
 const mongoose = require("mongoose")
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+
+function connectMongo() {
+    mongoose.connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+}
+
 const db = mongoose.connection
 db.on("error", error => console.error(error))
 db.once("open", () => console.log("Connected to Mongoose"))
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000, () => {
+    connectMongo()
+    console.log("App listening on port: 3000")
+})
