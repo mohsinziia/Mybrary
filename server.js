36a536a82e623a69a0e6baf3ae2dc0afc1dcc0f0
mohsinziia <ziamohsin409@gmail.com>
2023-08-08 01:29:15 +0500

    Initial Author Routes

--- server.js
+++ server.js
@@ -5,14 +5,18 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express")
 const app = express()
 const expressLayouts = require("express-ejs-layouts")
-const indexRouter = require("./routes/index") 
+const indexRouter = require("./routes/index")
+const authorRouter = require("./routes/authors")
 
 app.set("view engine", "ejs")
 app.set("views", __dirname + "/views")
 app.set("layout", "layouts/layout")
 app.use(expressLayouts)
 app.use(express.static("public"))
+app.use(express.urlencoded({limit: '10mb', extended: false}))
+app.use(express.json())
 app.use("/", indexRouter)
+app.use('/authors', authorRouter)
 
 const mongoose = require("mongoose")
 // const mongooseURI = "mongodb+srv://ziamohsin409:NfknfSa7x4zJ9LH7@mybrary.7x6n8s6.mongodb.net/?retryWrites=true&w=majority"
