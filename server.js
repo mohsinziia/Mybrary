3bbe6c0b449c755f1060db2cc576b0875dc0a8cd
mohsinziia <ziamohsin409@gmail.com>
2023-08-07 15:28:38 +0500

    small change

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ function connectMongo() {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
+    const db = mongoose.connection
     db.on("error", error => console.error(error))
     db.once("open", () => console.log("Connected to Mongoose"))
 }
 
-const db = mongoose.connection
 app.listen(process.env.PORT || 3000, () => {
     connectMongo()
     console.log("App listening on port: 3000")
