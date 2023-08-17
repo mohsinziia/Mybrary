9f5296b0b49d61571b60dbcfe8135642661e9257
mohsinziia <ziamohsin409@gmail.com>
2023-08-18 00:54:05 +0500

    Changes using chatgpt3

--- routes/books.js
+++ routes/books.js
@@ -1,41 +1,27 @@
 const express = require("express")
 const router = express.Router()
 const multer = require('multer')
+const multerS3 = require('multer-s3')
 const path = require('path')
 const fs = require('@cyclic.sh/s3fs')(process.env.CYCLIC_BUCKET_NAME)
 const Book = require('../models/book')
 const Author = require('../models/author')
 const uploadPath = path.join('public', Book.coverImageBasePath)
 const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
-// const upload = multer({
-//     dest: uploadPath,
-//     fileFilter: (req, file, callback) => {
-//         callback(null, imageMimeTypes.includes(file.mimetype))
-//     }
-// })
-
-// my code
-const AWS = require('aws-sdk')
-const s3 = new AWS.S3()
-async function addObject() {
-    return await s3.putObject({
-        Body: JSON.stringify({key:'value'}),
-        Bucket: process.env.CYCLIC_BUCKET_NAME,
-        Key: "some_files/my_file.json",
-    }).promise()
-}
-
-async function getObject() {
-    return await s3.getObject({
-        Bucket: process.env.CYCLIC_BUCKET_NAME,
-        Key: "some_files/my_file.json",
-    }).promise()
-}
-
-const addedObject = addObject()
-const gettedObject = getObject()
-console.log(addedObject)
-console.log(gettedObject)
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.CYCLIC_BUCKET_NAME,
+        acl: 'public-read',
+        key: (req, file, cb) => {
+          cb(null, Date.now().toString() + '-' + file.originalname);
+        }
+    }),
+    // dest: uploadPath,
+    fileFilter: (req, file, callback) => {
+        callback(null, imageMimeTypes.includes(file.mimetype))
+    }
+})
 
 // All Books Route
 router.get("/", async (req, res) => {
@@ -66,7 +52,7 @@ router.get('/new', async (req, res) => {
 })
 
 // Create Book Route
-router.post('/', async (req, res) => {
+router.post('/', upload.single('cover'), async (req, res) => {
     const fileName = req.file != null ? req.file.filename : null
     const book = new Book({
         title: req.body.title,
@@ -90,9 +76,9 @@ router.post('/', async (req, res) => {
 })
 
 function removeBookCover(fileName) {
-    // fs.unlink(path.join(uploadPath, fileName), err => {
-    //     if (err) console.error(err);
-    // })
+    fs.unlink(path.join('s3fs', fileName), err => {
+        if (err) console.error(err);
+    })
 }
 
 async function renderNewPage(res, book, hasError = true) {
