d93105f5ce4296f1e97c72f668555fbd103ce05b
mohsinziia <ziamohsin409@gmail.com>
2023-08-18 02:09:23 +0500

    O

--- routes/books.js
+++ routes/books.js
@@ -55,7 +55,7 @@ router.get('/new', async (req, res) => {
 
 // Create Book Route
 router.post('/', upload.single('cover'), async (req, res) => {
-    const fileName = req.file != null ? req.file.filename : null
+    const fileName = req.file != null ? req.file.originalname : null
     console.log(req.file)
     const book = new Book({
         title: req.body.title,
