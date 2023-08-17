28fb73dd3fdff9920148d5205de55a71efbc9917
mohsinziia <ziamohsin409@gmail.com>
2023-08-18 01:39:31 +0500

    Updating book model cover image base path

--- models/book.js
+++ models/book.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const path = require('path')
 
-const coverImageBasePath = 'uploads/bookCovers'
+const coverImageBasePath = 's3fs'
 
 const bookSchema = new mongoose.Schema({
     title: {
